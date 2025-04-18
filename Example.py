9396728d08f356b6fbdf54d718599105eeaece7d
sanchari1996 <sanchari.myindia@gmail.com>
2025-04-18 22:14:23 +0530

    testing commit

--- Example.py
+++ Example.py
@@ -1,5 +1,5 @@
 #you will read through and parse a file with text and numbers. You will extract all the numbers in the file and compute the sum of the numbers.
 import re
-print sum([int(i) for i in re.findall('[0-9]+',open("sum.txt").read())])
-
+print (sum([int(i) for i in re.findall('[0-9]+',open("sum.txt").read())]))
+print("Test")
 #Answer=445822
